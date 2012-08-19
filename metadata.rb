09cb5181f527b5bd894a0936249bfc0297a59e7d
John Dewey <john@dewey.ws>
2012-08-19 01:47:12 -0700

    + Added test-kitchen and minitest-hanlder tests
    Also added centos platform

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer_email "john@dewey.ws"
 license          "Apache 2.0"
 description      "Installs/Configures curl"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 
 recipe           "curl", "Installs/Configures curl"
 
-%w{ debian ubuntu }.each do |os|
+%w{ centos debian ubuntu }.each do |os|
   supports os
 end
